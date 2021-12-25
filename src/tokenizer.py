d7fc73ed465550eaffb8d2acb540d837b8f81f05
trag1c <xyakoo@yahoo.com>
2021-12-26 00:48:03 +0100

    Rewritten the tokenizer

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -2,80 +2,93 @@ from tokens import Token
 import handlers
 import sys
 
+Tokenizable = Token | str | int | None
 
-def tokenize(program: str) -> list[Token | int | str]:
-    tokens: list[Token | int | str] = []
-    temp = ""
-    string = False
-    multisemantic = "+-:<>=.?!&|~,"
+
+def tokenize(
+    program: str, *,
+    temp: str = "",
+    string: bool = False
+) -> list[Tokenizable]:
+
+    tokens: list[Tokenizable] = []
+    multisemantic = "+-:<>=.?!&|~,}{"
+    prev = ""
+
     for index, char in enumerate(program):
-        if not char.isalpha() and temp and not string:
-            tokens.append(temp)
-            temp = ""
-        elif char in "\n \t" and (char == "\n" or string):
-            tokens.append(char)
-        elif char != '"' and not string:
-            # Handling Names
-            if char.isalpha():
-                temp += char
-                continue
-            elif char == " " and temp.isalpha():
+
+        # String submitting
+        if char == '"' and prev != "\\":
+            if not string and temp:
                 tokens.append(temp)
                 temp = ""
-                continue
-
-            # Handling Multisemantic Tokens
-            if char in multisemantic:
-                handlers.init(program, index)
-                handler_list = {
-                    "+": handlers.plus,
-                    "-": handlers.minus,
-                    ":": handlers.colon,
-                    "<": handlers.less,
-                    ">": handlers.greater,
-                    "=": handlers.equal,
-                    ".": handlers.dot,
-                    "?": handlers.question,
-                    "!": handlers.exclamation,
-                    "&": handlers.ampersand,
-                    "|": handlers.pipe,
-                    "~": handlers.tilde,
-                    ",": handlers.comma
-                }
-                if out := handler_list[char]():
-                    tokens.append(out)
-            else:
-                digits = "/\\"
-                if char in digits:
-                    if program[index - 1] in digits:
-                        continue
-                    offset = 1
-                    while (digit := program[index + offset]) in digits:
-                        offset += 1
-                        char += digit
-                    tokens.append(
-                        int(char.translate({47: 49, 92: 48}), 2)
-                    )
-                elif not char.isspace():
-                    tokens.append(Token(char))
-        elif char == '"' and program[index - 1] != "\\":
-            # Handling Strings
-            if string:
-                tokens.append(f'"{temp}"')
-                temp = ""
+            temp += '"'
             string = not string
-        else:
+            if not string:
+                tokens.append(temp)
+                temp = ""
+
+        # String content and name handling
+        elif char.isalpha() or string:
             temp += char
+
+        # Namespace submitting
+        elif temp and not char.isalpha() and not string:
+            tokens.append(temp)
+            temp = ""
+            if x := tokenize(char + " ", temp=temp, string=string):
+                tokens.append(*x)
+
+        # Multisemantic token handling
+        elif char in multisemantic:
+            handlers.init(program, index)
+            handler_list = {
+                "+": handlers.plus,
+                "-": handlers.minus,
+                ":": handlers.colon,
+                "<": handlers.less,
+                ">": handlers.greater,
+                "=": handlers.equal,
+                ".": handlers.dot,
+                "?": handlers.question,
+                "!": handlers.exclamation,
+                "&": handlers.ampersand,
+                "|": handlers.pipe,
+                "~": handlers.tilde,
+                ",": handlers.comma,
+                "{": handlers.open_brace,
+                "}": handlers.close_brace
+            }
+            if out := handler_list[char]():
+                tokens.append(out)
+
+        # Number handling
+        elif char in "/\\":
+            if prev not in "/\\":
+                tokens.append(tokenize_number(program, index))
+
+        else:
+            try:
+                tokens.append(Token(char))
+            except ValueError:
+                pass
+
+        prev = char
+
     return tokens
 
 
-def main():
-    with open(sys.argv[1]) as f:
-        program = f.read()
+def tokenize_number(program: str, index: int) -> int:
+    temp = ""
+    for char in program[index:]:
+        if char in "/\\":
+            temp += char
+        else:
+            break
+    temp = temp.replace("/", "1").replace("\\", "0")
+    return int(temp, 2)
 
-    for token in tokenize(program):
-        print(token)
 
-
-if __name__ == "__main__":
-    main()
+with open(sys.argv[1]) as f:
+    for i in tokenize(f.read()):
+        print(i)
