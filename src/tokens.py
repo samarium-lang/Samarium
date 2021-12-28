b8a58583879409b9d34a3df8347e8af2094edbfd
trag1c <xyakoo@yahoo.com>
2021-12-28 19:58:53 +0100

    Grouped tokens

--- src/tokens.py
+++ src/tokens.py
@@ -2,20 +2,16 @@ from enum import Enum
 
 
 class Token(Enum):
+
+    # Arithmetic
     ADD = "+"
     SUB = "-"
     MUL = "++"
     DIV = "--"
     POW = "+++"
     MOD = "---"
-    SHR = ">>"
-    SHL = "<<"
 
-    ARRAY_OPEN = "["
-    ARRAY_CLOSE = "]"
-
-    ASSIGN = ":"
-
+    # Comparison
     LT = "<"
     GT = ">"
     LE = "<:"
@@ -23,50 +19,63 @@ class Token(Enum):
     EQ = "::"
     NE = ":::"
 
-    COMMENT = "=="
-    COMMENT_OPEN = "==<"
-    COMMENT_CLOSE = ">=="
-
-    IF = "?"
-    ELSE = ",,"
-    WHILE = ".."
-    FOR = "..."
-
-    FUNCTION = "*"
-    CLASS = "@"
-
-    STDIN = "???"
-    STDOUT = "!"
-
+    # Logical and Bitwise
     AND = "&&"
     OR = "||"
     NOT = "~~"
+
     BINAND = "&"
     BINOR = "|"
     BINNOT = "~"
     XOR = "^"
 
+    SHR = ">>"
+    SHL = "<<"
+
+    # Parens, Brackets and Braces
+    ARRAY_OPEN = "["
+    ARRAY_CLOSE = "]"
+
     BRACE_OPEN = "{"
     BRACE_CLOSE = "}"
-
-    TRY = "??"
-    CATCH = "!!"
-    THROW = "!!!"
-
-    ATTRIBUTE = "."
-    STRING = '"'
-    DOLLAR = "$"
-    LAMBDA = "#"
-    END = ";"
-    INSTANCE = "'"
-    DECORATOR = "%"
-    SEP = ","
-    TO = "->"
-    FROM = "<-"
-    IN = "->?"
 
     PAREN_OPEN = "("
     PAREN_CLOSE = ")"
 
     TABLE_OPEN = "{{"
     TABLE_CLOSE = "}}"
+
+    # Control Flow
+    IF = "?"
+    ELSE = ",,"
+    WHILE = ".."
+    FOR = "..."
+    TRY = "??"
+    CATCH = "!!"
+    THROW = "!!!"
+
+    # Comments
+    COMMENT = "=="
+    COMMENT_OPEN = "==<"
+    COMMENT_CLOSE = ">=="
+
+    # OOP
+    FUNCTION = "*"
+    CLASS = "@"
+    LAMBDA = "#"
+    INSTANCE = "'"
+    DECORATOR = "%"
+
+    # Other
+    ASSIGN = ":"
+    ATTRIBUTE = "."
+    DOLLAR = "$"
+    END = ";"
+    FROM = "<-"
+    IN = "->?"
+    RANDOM = "^^"
+    SEP = ","
+    STDIN = "???"
+    STDOUT = "!"
+    STRING = '"'
+    TO = "->"
