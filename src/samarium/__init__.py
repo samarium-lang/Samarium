9fd71ccf5ee9d2680f2c4b553a7d2955a08a9a47
trag1c <77130613+trag1c@users.noreply.github.com>
2022-02-24 21:49:37 +0100

    Added -c option

--- src/samarium/__init__.py
+++ src/samarium/__init__.py
@@ -8,13 +8,16 @@ MAIN = CodeHandler(globals())
 
 def main(debug: bool = False):
     if sys.argv[1] == "-v":
-        sys.exit(print("Samarium 0.1.0"))
-    try:
-        readfile(sys.argv[1])
-    except IOError:
-        print(f"file not found: {sys.argv[1]}")
-    with suppress(Exception, KeyboardInterrupt):
-        run(readfile(sys.argv[1]), MAIN, debug)
+        print("Samarium 0.1.0")
+    elif sys.argv[1] == "-c":
+        sys.exit(run(f"=> * {{{sys.argv[2]}!;}}", MAIN, debug))
+    else:
+        try:
+            readfile(sys.argv[1])
+        except IOError:
+            print(f"file not found: {sys.argv[1]}")
+        with suppress(Exception, KeyboardInterrupt):
+            run(readfile(sys.argv[1]), MAIN, debug)
 
 
 def main_debug():
