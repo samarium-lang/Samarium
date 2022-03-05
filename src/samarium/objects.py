80590f13d3d925f700caeb8627ae1710068a06eb
trag1c <77130613+trag1c@users.noreply.github.com>
2022-03-05 16:53:34 +0100

    Implemented removing elements from a table

--- src/samarium/objects.py
+++ src/samarium/objects.py
@@ -603,6 +603,15 @@ class Table(Class):
         self.value.update(other.value)
         return self
 
+    def _subtract_(self, other: String) -> Table:
+        c = self.value.copy()
+        del c[other]
+        return Table(c)
+
+    def _subtractAssign_(self, other: String) -> Table:
+        del self.value[other]
+        return self
+
 
 class Array(Class):
 
