6df7b25c6622616277426e66f8a22010741bfbc9
trag1c <77130613+trag1c@users.noreply.github.com>
2022-03-09 23:31:18 +0100

    Refactored template

--- src/samarium/template.py
+++ src/samarium/template.py
@@ -7,14 +7,15 @@ sys.stdout = open(os.devnull, "w")
 if __name__ == "samarium":
     sys.stdout = STDOUT
     argv = Array([String(i) for i in sys.argv[1:]])
-    if len(signature(entry).parameters) > 1:
+    params = len(signature(entry).parameters)
+    if not params:
+        ex = entry()
+    elif params == 1:
+        ex = entry(argv)
+    else:
         handle_exception(
             SamariumSyntaxError(
                 "main function should take 0 or 1 arguments"
             )
         )
-    try:
-        ex = entry(argv)
-    except TypeError:
-        ex = entry()
     sys.exit(ex.value)
