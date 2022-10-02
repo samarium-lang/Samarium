0fd28eecd9e161c7b81823bdbb7f4fd466d7d2e0
Endercheif <45527309+Endercheif@users.noreply.github.com>
2022-10-02 16:38:54 -0700

    rename to follow PEP8

--- src/samarium/__init__.py
+++ src/samarium/__init__.py
@@ -2,7 +2,7 @@ import sys
 from contextlib import suppress
 
 from .core import readfile, run
-from .shell import SamariumRepl
+from .shell import SamariumREPL
 
 from .transpiler import Registry
 from .utils import __version__
@@ -22,7 +22,7 @@ file              : reads program from script file"""
 def main(debug: bool = False):
 
     if len(sys.argv) == 1:
-        return SamariumRepl(debug).run()
+        return SamariumREPL(debug).run()
 
     if (arg := sys.argv[1]) in OPTIONS:
         if arg in OPTIONS[:2]:
