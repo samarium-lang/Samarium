9212e16a641962181dea8f5d4544361c80a5b720
Lunarmagpie <bambolambo0@gmail.com>
2022-10-06 14:38:38 -0400

    fix file path

--- tests/samarium/test_run.py
+++ tests/samarium/test_run.py
@@ -2,14 +2,14 @@ from tests import Samarium
 
 
 def test_basics():
-    with Samarium(file="tests/hello_world.sm") as s:
+    with Samarium(file="tests/samarium/hello_world.sm") as s:
         assert s.stdout == "Hello, World!\n"
         assert not s.stderr
         s.assert_return_code(0)
 
 
 def test_input():
-    with Samarium(file="tests/input.sm") as s:
+    with Samarium(file="tests/samarium/input.sm") as s:
         s.write("Hello, World!\n")
         assert s.stdout == "Hello, World!\n"
         assert not s.stderr
