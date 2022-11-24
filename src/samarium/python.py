02c459fd7d5b3c99e2b7a276399c876d897af066
Endercheif <reet22singh@gmail.com>
2022-11-23 21:08:14 -0800

    fix: added more converters

--- src/samarium/python.py
+++ src/samarium/python.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from collections.abc import Callable
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, TypeVar, Iterable as PyIterable
+from io import BufferedIOBase, IOBase
 from functools import wraps
 from samarium.classes import (
     NULL,
@@ -10,8 +11,12 @@ from samarium.classes import (
     Null,
     String,
     Table,
+    Iterator,
+    Slice,
+    File,
+    Attrs
 )
-from samarium.classes.base import Attrs
+from samarium.classes.base import Zip
 
 F = TypeVar("F", bound=Callable)
 
@@ -27,7 +32,7 @@ def export(func: F) -> PythonExport[F]:
 
 
 def to_python(obj: Attrs) -> object:
-    if isinstance(obj, (String, Integer)):
+    if isinstance(obj, (String, Integer, Zip, File)):
         return obj.val
     elif isinstance(obj, Null):
         return None
@@ -35,10 +40,14 @@ def to_python(obj: Attrs) -> object:
         return [to_python(i) for i in obj.val]
     elif isinstance(obj, Table):
         return {to_python(k): to_python(v) for k, v in obj.val.items()}
+    elif isinstance(obj, Slice):
+        return range(obj.start.val, obj.stop.val, obj.step.val)
+    elif isinstance(obj, Iterator):
+        return (to_python(i) for i in obj)
 
 
 def to_samarium(obj: object) -> Attrs | type[Attrs]:
-    if isinstance(obj, (int, bool)):
+    if isinstance(obj, (int, bool, float)):
         return Integer(obj)
     elif isinstance(obj, str):
         return String(obj)
@@ -48,6 +57,14 @@ def to_samarium(obj: object) -> Attrs | type[Attrs]:
         return Array([to_samarium(i) for i in obj])
     elif isinstance(obj, dict):
         return Table({to_samarium(k): to_samarium(v) for k, v in obj.items()})
+    elif isinstance(obj, range):
+        return Slice(to_samarium(range.start), to_samarium(range.stop), to_samarium(range.step))
+    elif isinstance(obj, IOBase): 
+        return File(obj, obj.mode, obj.name, isinstance(obj, BufferedIOBase)) # type: ignore
+    elif isinstance(obj, zip):
+        return Zip(*obj)
+    elif isinstance(obj, PyIterable):
+        return Iterator(obj)
 
 
 def function(func):
