077c16b76c8dd51a18dafe3c86d839eb62af7ad1
trag1c <77130613+trag1c@users.noreply.github.com>
2022-11-28 03:32:47 +0100

    Simplified super call

--- src/samarium/utils.py
+++ src/samarium/utils.py
@@ -44,7 +44,7 @@ class Singleton:
 
     def __new__(cls, *args: Any, **kwargs: Any):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__new__(cls, *args, **kwargs)
+            cls._instances[cls] = super().__new__(cls, *args, **kwargs)
         return cls._instances[cls]
 
 
