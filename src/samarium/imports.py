92b9387e6a6d83eb716ff9e32aae94d7ef4790aa
trag1c <77130613+trag1c@users.noreply.github.com>
2022-12-07 08:35:21 +0100

    Moved math.is_prime to Python

--- src/samarium/imports.py
+++ src/samarium/imports.py
@@ -94,7 +94,7 @@ def resolve_path(name: str) -> Path:
         path = Path().resolve()
     paths = [e.name for e in path.iterdir()]
     if not (f"{name}.sm" in paths or f"{name}.py" in paths):
-        if name not in MODULE_NAMES:
+        if name not in MODULE_NAMES + ["std_py2sm"]:
             raise SamariumImportError(f"invalid module: {name}")
         path = Path(__file__).resolve().parent / "modules"
     return path
