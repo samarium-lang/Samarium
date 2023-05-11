6801427c76310d61c6ceb7e3904c2be7a846b242
trag1c <trag1cdev@yahoo.com>
2023-05-11 21:35:47 +0200

    Removed redundant semicolon

--- src/samarium/__init__.py
+++ src/samarium/__init__.py
@@ -29,7 +29,7 @@ def main(*, debug: bool = False) -> None:
             print(f"Samarium {__version__}")
         elif arg in OPTIONS[2:4]:
             if len(sys.argv) > 2:
-                run(sys.argv[2] + " !;", reg, arg, debug=debug)
+                run(sys.argv[2] + " !", reg, arg, debug=debug)
             DAHLIA.print("&4missing code to execute", file=sys.stderr)
         elif arg in OPTIONS[4:]:
             print(HELP)
