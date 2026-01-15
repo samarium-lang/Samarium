be974105af2672ef4b8b8dd1e86f012a32b5cfea
trag1c <dev@jakubr.me>
2026-01-16 00:34:58 +0100

    add postfix attr/call/op tests

--- tests/parser/test_parse_postfix.py
+++ tests/parser/test_parse_postfix.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 
 from samarium import nodes as n
@@ -24,6 +25,84 @@ def test_parse_postfix_call(inner_source: str, args: list[n.Expr]) -> None:
     ]
 
 
+def test_parse_postfix_repeated_call() -> None:
+    assert Parser("()()()();").parse() == [
+        n.ExprStmt(
+            n.Postfix(
+                n.Postfix(n.Postfix(n.UnitExpr.NULL, n.Call([])), n.Call([])),
+                n.Call([]),
+            )
+        )
+    ]
+
+
 def test_parse_postfix_call_fail() -> None:
     with pytest.raises(ParseError, match="expected `,` between arguments"):
         _ = Parser("()(a b);").parse()
+
+
+def test_parse_postfix_attr() -> None:
+    assert Parser("3.14;").parse() == [
+        n.ExprStmt(n.Postfix(n.Identifier("3"), n.Attribute(n.Identifier("14"))))
+    ]
+
+
+@pytest.mark.parametrize(
+    ("op", "postfix_kind"),
+    [
+        ("???", n.UnitPostfix.READLINE),
+        ("??", n.UnitPostfix.RANDOM),
+        ("?!", n.UnitPostfix.TYPE),
+        ("!", n.UnitPostfix.PRINT),
+        ("!?", n.UnitPostfix.PARENT),
+        ("##", n.UnitPostfix.HASH),
+        ("$", n.UnitPostfix.SPECIAL),
+        ("**", n.UnitPostfix.ID),
+        ("%", n.UnitPostfix.CAST),
+    ],
+)
+def test_parse_postfix_op(op: str, postfix_kind: n.UnitPostfix) -> None:
+    assert Parser(f"(){op};").parse() == [
+        n.ExprStmt(n.Postfix(n.UnitExpr.NULL, postfix_kind))
+    ]
+
+
+def test_parse_postfix_op_multiple() -> None:
+    assert Parser(f"()?????!?!$?!**%##;").parse() == [
+        n.ExprStmt(
+            n.Postfix(
+                n.Postfix(
+                    n.Postfix(
+                        n.Postfix(
+                            n.Postfix(
+                                n.Postfix(
+                                    n.Postfix(
+                                        n.Postfix(
+                                            n.Postfix(
+                                                n.UnitExpr.NULL, n.UnitPostfix.READLINE
+                                            ),
+                                            n.UnitPostfix.RANDOM,
+                                        ),
+                                        n.UnitPostfix.PARENT,
+                                    ),
+                                    n.UnitPostfix.PRINT,
+                                ),
+                                n.UnitPostfix.SPECIAL,
+                            ),
+                            n.UnitPostfix.TYPE,
+                        ),
+                        n.UnitPostfix.ID,
+                    ),
+                    n.UnitPostfix.CAST,
+                ),
+                n.UnitPostfix.HASH,
+            )
+        )
+    ]
+
+
+def test_parse_postfix_attr_fail() -> None:
+    with pytest.raises(
+        ParseError, match=re.escape("expected attribute name after `.`")
+    ):
+        _ = Parser("a.;").parse()
